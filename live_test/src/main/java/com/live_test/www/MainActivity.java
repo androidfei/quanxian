837c17b1e09369eb3b822a24d8309da4a372d796
zhangxiongfei <androidfei@163.com>
2017-05-19 18:27:54 +0800

    修改readme

--- live_test/src/main/java/com/live_test/www/MainActivity.java
+++ live_test/src/main/java/com/live_test/www/MainActivity.java
@@ -1,13 +1,31 @@
 package com.live_test.www;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewTreeObserver;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TestView test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        test = (TestView) findViewById(R.id.text);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Handler handler = new Handler();
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                test.start_an();
+            }
+        };
+        handler.postDelayed(runnable, 1000);
     }
 }
